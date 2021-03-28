9e2ae6c7fca98d63019ebc6307dc499c284481c6
Jashin Jashua <71561406+jashinjashua@users.noreply.github.com>
2021-03-28 19:00:30 +0300

    Update obstacles.js

--- obstacles.js
+++ obstacles.js
@@ -7,52 +7,52 @@ const pipet = new Image();
 pipet.src = "image/pipet.png";
 
 class Obstacle {
-    constructor() {
-        this.reset()
-    }
-    reset() {
-        this.top = (Math.random() * canvas.height / 2.5) + 25;
-        this.bottom = (Math.random() * canvas.height / 2.5) + 25;
-        this.x = canvas.width;
-        this.width = 50;
-        this.counted = false;
+  constructor() {
+    this.reset();
+  }
+  reset() {
+    this.top = (Math.random() * canvas.height) / 2.5 + 25;
+    this.bottom = (Math.random() * canvas.height) / 2.5 + 25;
+    this.x = canvas.width;
+    this.width = 50;
+    this.counted = false;
+  }
+
+  draw() {
+    ctx.drawImage(pipet, this.x, 0, this.width, this.top);
+    ctx.drawImage(
+      pipeb,
+      this.x,
+      canvas.height - this.bottom,
+      this.width,
+      this.bottom
+    );
+  }
+
+  update() {
+    this.x -= gamespeed;
+    if (!this.counted && this.x < bird.x) {
+      score++;
+      this.counted = true;
     }
 
-    draw() {
-        ctx.drawImage(pipet, this.x, 0, this.width, this.top);
-        ctx.drawImage(pipeb, this.x, canvas.height - this.bottom, this.width, this.bottom);
-    }
-
-    update() {
-        this.x -= gamespeed;
-        if (!this.counted && this.x < bird.x) {
-            score++;
-            this.counted = true;
-        }
-
-        this.draw();
-    }
-
+    this.draw();
+  }
 }
 
 function resetObstacles() {
-    for (let i = 0; i < obstaclesArray.length; i++) {
-        obstaclesArray[i].reset();
-
-    }
+  obstaclesArray.length = [];
 }
 
 function handleObstacles() {
-    if (frame % 75 === 0) {
-        obstaclesArray.unshift(new Obstacle);
-    }
-    for (let i = 0; i < obstaclesArray.length; i++) {
-        obstaclesArray[i].update();
-    }
+  if (frame % 75 === 0) {
+    obstaclesArray.unshift(new Obstacle());
+  }
+  for (let i = 0; i < obstaclesArray.length; i++) {
+    obstaclesArray[i].update();
+  }
 
-    if (obstaclesArray > 20) {
-        obstaclesArray.pop(obstaclesArray[0]);
-
-    }
-
+  if (obstaclesArray > 20) {
+    obstaclesArray.pop(obstaclesArray[0]);
+  }
 }
